009d5650753cedbb1f9d421150775b3fad042205
Omar Benmegdoul <omarbenmeg@gmail.com>
2021-05-04 15:03:18 -0400

    added fb client

---
+++ src/firebase.js
@@ -0,0 +1,34 @@
+import firebase from 'firebase';
+
+
+const env = process.env;
+console.log('❗ firebase.js - line:5 `env` (type:',typeof env,')',env);
+
+const credentials = ""// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: env.measurementId,
+//   authDomain: env.authDomain,
+//   projectId: env.projectId,
+//   storageBucket: env.storageBucket,
+//   messagingSenderId: env.messagingSenderId,
+//   appId: env.appId,
+//   measurementId: env.measurementId
+// };
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const fbConfig = {
+  apiKey: "AIzaSyAzXYi9BGHB2obQnsFeati11AQRCuAujP0",
+  authDomain: "my-todo-a3582.firebaseapp.com",
+  projectId: "my-todo-a3582",
+  storageBucket: "my-todo-a3582.appspot.com",
+  messagingSenderId: "42004174450",
+  appId: "1:42004174450:web:20b013b45ce10d826254eb",
+  measurementId: "G-RV3XS96QV3"
+};
+
+console.log('❗ firebase.js - line:29 `fbConfig` (type:',typeof fbConfig,')',fbConfig);
+
+
+firebase.initializeApp(fbConfig);
+
+export const db = firebase.app().firestore();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from "react-dom";
-import App from "./App"
+import App from "./App";
+import {} from "./firebase"
 
 const rootDiv = document.getElementById("root"); 
 ReactDOM.render(<App />, rootDiv)
